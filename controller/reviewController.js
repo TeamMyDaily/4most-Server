dae47c1a096aa034e120c8a5b965cd2e9facaa9b
Kayoung <rkdkdudjd@naver.com>
2021-01-11 01:07:41 +0900

    [FIX] #14 - ReviewController

--- controller/reviewController.js
+++ controller/reviewController.js
@@ -7,57 +7,7 @@ const { Op } = require('sequelize');
 module.exports = {
   readOne: async (req, res) => {
     const { start, end } = req.query;
-    const userId = req.decoded;
-    // const userId = 1;
-    if (!userId || !start || !end) {
-      console.log('필요한 정보가 없습니다.');
-      return res
-        .status(sc.BAD_REQUEST)
-        .send(ut.fail(sc.BAD_REQUEST, rm.NULL_VALUE));
-    }
-    try {
-      const user = await User.findOne({ where: {id: userId} });
-      if (!user) {
-        console.log('사용자를 찾을 수 없음');
-        return res
-          .status(sc.BAD_REQUEST)
-          .send(ut.fail(sc.BAD_REQUEST, rm.NO_USER));
-      }
-
-      const startDate = new Date(+start);
-      const endDate = new Date(+end);
-      const result = {};
-      const review = await Review.findOne({
-        raw: true,
-        where: {
-          UserId: userId,
-          date: {[Op.gte]: startDate, [Op.lte]: endDate}
-        }
-      });
-      if (!review) {
-        result.isWritten = false;
-        return res
-                .status(sc.OK)
-                .send(ut.success(sc.OK, '주차별 리뷰 조회 성공', result));
-      }
-      result.isWritten = true;
-      result.review = review;
-
-      return res
-              .status(sc.OK)
-              .send(ut.success(sc.OK, '주차별 리뷰 조회 성공', result));
-    } catch (err) {
-      console.log(err);
-      return res
-              .status(sc.INTERNAL_SERVER_ERROR)
-              .send(ut.fail(sc.INTERNAL_SERVER_ERROR, rm.INTERNAL_SERVER_ERROR));
-    }
-
-  },
-  createOne: async (req, res) => {
-    const { start, end, now, good, bad, next } = req.body;
-    const { id } = req.decoded;
-    console.log(req.body);
+    const { id }  = req.decoded;
     // const id = 1;
     if (!id || !start || !end) {
       console.log('필요한 정보가 없습니다.');
@@ -66,25 +16,130 @@ module.exports = {
         .send(ut.fail(sc.BAD_REQUEST, rm.NULL_VALUE));
     }
     try {
-      const user = await User.findOne({ where: {id: id} });
-      if (!user) {
-        console.log('사용자를 찾을 수 없음');
+      const startDate = new Date(+start);
+      const endDate = new Date(+end);
+      const result = {};
+      console.log(id);
+      const review = await Review.findOne({
+        attributes:['good', 'bad', 'next'],
+        where: {
+          UserId: id,
+          date: {[Op.gte]: startDate, [Op.lte]: endDate}
+        }
+      });
+
+      if (!review) {
+        result.isWritten = false;
         return res
-          .status(sc.BAD_REQUEST)
-          .send(ut.fail(sc.BAD_REQUEST, rm.NO_USER));
+                .status(sc.OK)
+                .send(ut.success(sc.OK, '주차별 회고 조회 성공', result));
       }
+      result.isWritten = true;
+      result.review = review;
+
+      return res
+              .status(sc.OK)
+              .send(ut.success(sc.OK, '주차별 회고 조회 성공', result));
+    } catch (err) {
+      console.log(err);
+      return res
+              .status(sc.INTERNAL_SERVER_ERROR)
+              .send(ut.fail(sc.INTERNAL_SERVER_ERROR, rm.INTERNAL_SERVER_ERROR));
+    }
+
+  },
+  createOrUpdateOne: async (req, res) => {
+    const { start, end, now, subType, content } = req.body;
+    const { id } = req.decoded;
+    
+    // const id = 1;
+    if (!id || !start || !end || !subType || !content) {
+      console.log('필요한 정보가 없습니다.');
+      return res
+        .status(sc.BAD_REQUEST)
+        .send(ut.fail(sc.BAD_REQUEST, rm.NULL_VALUE));
+    }
+    if (subType!==1 && subType!==2 && subType!==3) {
+      console.log('type값 오류');
+      return res
+        .status(sc.BAD_REQUEST)
+        .send(util.fail(sc.BAD_REQUEST, 'subType 범위 이상'));
+    }
+    try {
       const startDate = new Date(+start);
       const endDate = new Date(+end);
       const nowDate = new Date(+now);
 
-      let inputDate;
-      //이렇게 해도 괜찮은걸까
-      if (startDate <= nowDate && nowDate <= endDate){
-        inputDate = nowDate
+      const alreadyWritten = await Review.findOne({
+        where: {
+          date: {
+            [Op.gte]: startDate,
+            [Op.lte]: endDate
+          }
+        }
+      });
+
+      let createdReview;
+      if(!alreadyWritten) { // create new review
+        let inputDate;
+        if (startDate <= nowDate && nowDate <= endDate){
+          inputDate = nowDate
+        } else {
+          inputDate = endDate
+        }
+        switch(subType) {
+          case 1: 
+            createdReview = await Review.create({ UserId: id, date: inputDate, good: content});
+            break;
+          case 2:
+            createdReview = await Review.create({ UserId: id, date: inputDate, bad: content });
+            break;
+          case 3:
+            createdReview = await Review.create({ UserId: id, date: inputDate, next: content });
+            break;
+        }
+        
       } else {
-        inputDate = endDate
+        switch(subType) {
+          case 1: 
+            await Review.update({ good: content }, {
+              where: {
+                date: {
+                  [Op.gte]: startDate,
+                  [Op.lte]: endDate,
+                }
+              } 
+            });
+            break;
+          case 2:
+            await Review.update({ bad: content }, {
+              where: {
+                date: {
+                  [Op.gte]: startDate,
+                  [Op.lte]: endDate,
+                }
+              } 
+            });
+            break;
+          case 3:
+            await Review.update({ next: content }, {
+              where: {
+                date: {
+                  [Op.gte]: startDate,
+                  [Op.lte]: endDate,
+                }
+              } 
+            });
+            break;
+        } 
+        createdReview = await Review.findOne({
+          attributes: ['good', 'bad', 'next'],
+          where: {
+            UserId: id,
+            date: {[Op.gte]: startDate, [Op.lte]: endDate}
+          }
+        });
       }
-      const createdReview = await Review.create({ UserId: id, date: inputDate, good: good, bad: bad, next: next });
       return res
         .status(sc.OK)
         .send(ut.success(sc.OK, '회고 등록 완료', createdReview));
--- routes/reviews/index.js
+++ routes/reviews/index.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const reviewController = require('../../controller/reviewController.js')
 
 router.get('/', reviewController.readOne);
-router.post('/', reviewController.createOne);
+router.post('/', reviewController.createOrUpdateOne);
 
 module.exports = router;
