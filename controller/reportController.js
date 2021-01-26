8850eb3d15fc1bb867a973b94c55b669f377e466
Kayoung <rkdkdudjd@naver.com>
2021-01-26 21:59:37 +0900

    [FIX] #37 - refactoring var names

--- controller/reportController.js
+++ controller/reportController.js
@@ -5,40 +5,20 @@ const rm = require('../modules/responseMessage');
 const { Op } = require('sequelize');
 
 module.exports = {
-  // GET /reports?startYear=20
   readWeek: async (req, res) => {
     const { start, end }  = req.query;
-
     const { id } = req.decoded;
-    // const id = 1; //development
+    // const id = 1; // development
 
     if (!id || !start || !end) {
       console.log('필요한 정보가 없습니다.');
-      return res
-        .status(sc.BAD_REQUEST)
-        .send(ut.fail(sc.BAD_REQUEST, rm.NULL_VALUE));
+      return res.status(sc.BAD_REQUEST).send(ut.fail(sc.BAD_REQUEST, rm.NULL_VALUE));
     }
-    //무조건 문자로 넘어와서 숫자로 변경 해주어야 함
     const startDate = new Date(start*1);
     const endDate = new Date(end*1);
     try {
-      const user = await User.findOne({ where: {id: id} });
-      if (!user) {
-        console.log('사용자를 찾을 수 없음');
-        return res
-          .status(sc.BAD_REQUEST)
-          .send(ut.fail(sc.BAD_REQUEST, rm.NO_USER));
-      }   
-      const totalKeywords = await TotalKeyword.findAll({
-        where: {
-          UserId: id
-        }
-      });
-      
+      const totalKeywords = await TotalKeyword.findAll({ where: { UserId: id } });
       const totalKeywordIds = totalKeywords.map(e => e.id);
-      
-      const data = new Object();
-
       const mostRecentDate = await KeywordByDate.findAll({
         limit: 1,
         attributes: ['date'],
@@ -50,17 +30,16 @@ module.exports = {
         },
         order: [['date', 'DESC']]
       });
-      console.log(mostRecentDate);
+      const resBody = new Object();
       if(!mostRecentDate[0]) {
-        data.keywordsExist = false;
+        resBody.keywordsExist = false;
         return res
           .status(sc.OK)
-          .send(ut.success(sc.OK, rm.READ_REPORT_SUCCESS, data));
+          .send(ut.success(sc.OK, rm.READ_REPORT_SUCCESS, resBody));
       }
       
       //여기서 그 주차에 있었던 키워드들은 다 가지고 와야 함
       const queryResult = await KeywordByDate.findAll({
-        //raw: true,
         attributes: { exclude: ['id'] },
         where: {
           date : { [Op.lt]: endDate , [Op.gte]: startDate }
@@ -126,14 +105,14 @@ module.exports = {
           }
         }
       }
-      data.keywordsExist = true;
+      resBody.keywordsExist = true;
       //결과 배열로 변환해서 response에 넣어주기 위해 한 번 더 반복문 실행
-      data.result = new Array();
+      resBody.result = new Array();
       for (let keyword of Object.keys(results)) {
-        data.result.push(results[keyword]);
+        resBody.result.push(results[keyword]);
       }
-      console.log(data);
-      return res.status(sc.OK).send(ut.success(sc.OK, rm.READ_REPORT_SUCCESS, data));
+      console.log(resBody);
+      return res.status(sc.OK).send(ut.success(sc.OK, rm.READ_REPORT_SUCCESS, resBody));
       
     } catch (err) {
       console.log(err);
@@ -153,24 +132,17 @@ module.exports = {
     }
   
     const startDate = new Date(+start), endDate = new Date(+end);
-
-
-    //한 개의 키워드에 대해서!!
     try {
       const rawKeywordName = await TotalKeyword.findOne({
-        attributes: [],
-        include: [{
-          model: Keyword,
-          attributes: ['name']
-        }],
-        where: {
-          id: totalKeywordId,
-        }
+        include: [
+          {
+            model: Keyword,
+            attributes: ['name']
+          }
+        ],
+        where: { id: totalKeywordId }
       });
 
-      const keywordName = rawKeywordName.Keyword.name;
-
-      
       const weekGoal = await WeekGoal.findAll({
         raw: true,
         limit: 1,
@@ -183,48 +155,41 @@ module.exports = {
       
       const totalKeywords = await TotalKeyword.findAll({
         raw: true,
-        where: {
-          id: totalKeywordId
-        },
+        where: { id: totalKeywordId },
         include: [
           {
             model: Task,
             attributes: ['id', 'title', 'date', 'satisfaction'],
             where: {
-              date: {
-                [Op.gte]: startDate, [Op.lt]: endDate,
-              }
+              date: { [Op.gte]: startDate, [Op.lt]: endDate, }
             }
           }
         ]
       });
-
-      console.log()
-      const result = {};
-      result.totalKeywordId = totalKeywordId;
-      
-      result.keywordName = keywordName;
-      if(weekGoal.length === 0) {
-        result.goalExist = false;
-        result.goal = null;
-      }else {
-        result.goalExist = true;
-        result.weekGoalId = weekGoal[0]['id'];
-        result.goal = weekGoal[0]['goal'];
-        result.isGoalCompleted = !!weekGoal[0]['isGoalCompleted'];
+      // response body 채우기
+      const resBody = {};
+      resBody.totalKeywordId = totalKeywordId;
+      resBody.keywordName = rawKeywordName.Keyword.name;
+      if (weekGoal.length) { // 0
+        resBody.goalExist = false;
+        resBody.goal = null;
+      } else {
+        resBody.goalExist = true;
+        resBody.weekGoalId = weekGoal[0]['id'];
+        resBody.goal = weekGoal[0]['goal'];
+        resBody.isGoalCompleted = !!weekGoal[0]['isGoalCompleted'];
       }
     
-      result.tasks = [];
+      resBody.tasks = [];
       totalKeywords.forEach(o => {
         let task = {};
         task.taskId = o['Tasks.id'];
         task.title = o['Tasks.title'];
         task.date = o['Tasks.date'];
         task.satisfaction = o['Tasks.satisfaction'];
-        result.tasks.push(task);
+        resBody.tasks.push(task);
       })
-      
-      return res.status(sc.OK).send(ut.success(sc.OK, '리포트 > 키워드별 조회 성공', result));  
+      return res.status(sc.OK).send(ut.success(sc.OK, '리포트 > 키워드별 조회 성공', resBody));  
     } catch (err) {
       console.log(err);
       return res .status(sc.INTERNAL_SERVER_ERROR).send(ut.fail(sc.INTERNAL_SERVER_ERROR, rm.INTERNAL_SERVER_ERROR));
